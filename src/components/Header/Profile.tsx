64b5f9fe693a3392728d63189fa456b9d14628c7
wellington <wellington@liquidworks.com.br>
2022-05-10 23:47:14 -0300

    Sidebar drawer  com context

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,12 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile(){
+interface ProfileProps {
+    showProfileData: boolean
+}
+
+export function Profile({showProfileData}: ProfileProps){
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text >Wellington willers</Text>
-                <Text color="gray.300" fontSize="small">Wellington.willers@gmail.com</Text>
-            </Box>
+            {
+                showProfileData && (
+                    <Box mr="4" textAlign="right">
+                        <Text >Wellington willers</Text>
+                        <Text color="gray.300" fontSize="small">Wellington.willers@gmail.com</Text>
+                    </Box>
+                )
+            }
 
             <Avatar size="md" name="wellington willers" />
         </Flex>
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,48 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react"
 import { NotificationNav } from "./NotificationsNav"
 import { Profile } from "./Profile"
 import { SearchBox } from "./SearchBox"
 import { Logo } from "./Logo"
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext"
+import { RiMenuLine } from "react-icons/ri"
 
 
 export function Header() {
+
+    const {onOpen} = useSidebarDrawer()
+
+    const showInLarge = useBreakpointValue({
+        base: false,
+        lg: true
+    })
+
     return (
         <Flex as="header" w="100%" maxWidth={1480} h="20" mx="auto" mt="4" px="6" align="center">
+            {
+                !showInLarge && (
+                    <IconButton 
+                        aria-label="Open navigation"
+                        icon={<Icon as={RiMenuLine} />}
+                        fontSize="24"
+                        variant="unstyled"
+                        onClick={onOpen}
+                        mr="2"
+                    ></IconButton>
+                )
+            }
+
             <Logo />
 
-            <SearchBox />
+            {
+                showInLarge && (
+                    <SearchBox />
+                )
+            }
 
             <Flex align="center" ml="auto">
                 <NotificationNav />
 
-                <Profile />
+                <Profile showProfileData={showInLarge} />
             </Flex>
         </Flex>
     )
